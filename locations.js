3bd269cf3235f55d7d5c49aed91b2dde66a34650
MoneyFreedomWeb <moneyfreedomk@gmail.com>
2025-10-24 01:24:50 +0200

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,23 @@
+const LOCATIONS = {
+    'bar-centrale': {
+        name: 'Bar Centrale',
+        emoji: '☕',
+        promoText: 'Grazie per aver visitato il Bar Centrale! Scansionando questo QR code hai accesso ad un\'offerta esclusiva: ogni caffè viene servito con un cornetto artigianale gratuito fino a fine mese. Unisciti alla nostra community per scoprire tutte le promozioni!',
+        discount: '🎁 Caffè + Cornetto Gratis'
+    },
+    'pizzeria-napoli': {
+        name: 'Pizzeria Napoli',
+        emoji: '🍕',
+        promoText: 'Benvenuto alla Pizzeria Napoli! Per ringraziarti della visita, ti offriamo uno sconto del 20% sulla tua prima pizza d\'asporto. Segui i nostri canali social per scoprire le pizze speciali della settimana e le serate a tema napoletano!',
+        discount: '🎉 20% di Sconto Prima Pizza'
+    },
+    'biblioteca-comunale': {
+        name: 'Biblioteca Comunale',
+        emoji: '📚',
+        promoText: 'Benvenuto in Biblioteca! Scopri i nostri eventi culturali settimanali, presentazioni di libri con autori locali e gruppi di lettura tematici. Unisciti alla community per ricevere aggiornamenti su nuovi arrivi e attività culturali esclusive.',
+        discount: '📖 Tessera Digitale Gratuita'
+    },
+    'bar-sport': {
+        name: 'Bar Sport',
+        emoji: '⚽',
+        promoText: 'Benvenuto al Bar Sport! Il luogo ideale per vedere le partite in compag
